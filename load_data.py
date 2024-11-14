4bc710b15ae7e1286060aabea4ee5a936116807e
swetha-624 <32007690+swetha-624@users.noreply.github.com>
2024-11-14 15:40:16 -0500

    loading data
    loading data

---
+++ load_data.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+# Replace the URL with the raw GitHub file URL
+url = 'https://github.com/swetha-624/customer-churn-analysis/blob/main/WA_Fn-UseC_-Telco-Customer-Churn.csv'
+# Load the dataset
+df = pd.read_csv(url)
+
+# Display the first few rows of the dataset
+# print(df.head()) " 
